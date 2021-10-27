300a7b5ead5ec09d4250bf965687f5a0c64c015e
tilankamihingu <tilankamihingu2018@gmail.com>
2021-10-27 11:53:52 -0700

    services created

---
+++ src/main/java/com/tilanka/employeemanager/service/EmployeeService.java
@@ -0,0 +1,42 @@
+package com.tilanka.employeemanager.service;
+
+import com.tilanka.employeemanager.exception.UserNotFoundException;
+import com.tilanka.employeemanager.model.Employee;
+import com.tilanka.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployee(){
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id){
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("user by id" + id + "not found" ));
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
+    }
+}
